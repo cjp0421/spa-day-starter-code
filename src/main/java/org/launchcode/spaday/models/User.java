d70981b81eb4df8b29a11236a0957c3aef3fb349
cjp0421 <76797183+cjp0421@users.noreply.github.com>
2023-05-10 12:37:08 -0500

    add get mapping for the index page, which was not displaying. commented out the user greeting on index template to make it work - needs fixing

--- src/main/java/org/launchcode/spaday/models/User.java
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -1,13 +1,20 @@
 package org.launchcode.spaday.models;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 public class User {
+    @NotBlank(message="Username cannot be blank.")
+    @Size(min=5, max=15, message="Username must be between 5 and 15 characters.")
     private String username;
+    @Email(message="Invalid email.")
     private String email;
+
+    @Size(min=6)
     private String password;
 
-    public User() {
-
-    }
+    public User() {}
 
     public User(String username, String email, String password) {
         this();
